c9f5a9c4e7981856ec53ef8c19f67d6f1f27bf48
Elham Ahmadzadeh <elham.ahmadzadeh@sigmatechnology.se>
2022-03-01 12:15:01 +0100

    added base functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import QuestinonCard from './components/QuestinonCard';
 
 function App() {
+  const startTrivia = async () => {
+
+  }
+  const chackAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+  const nextQuestion = () => {
+
+  }
   return (
     <div className="App">
-    Quizz
+      <h1>React Quiz</h1>
+      <button onClick={startTrivia}>Start</button>
+      <p className='score'>Score :</p>
+      <p>Loading quetions ...</p>
+      <QuestinonCard/>
+      <button className='next' onClick={nextQuestion}>Next Qustion</button>
     </div>
   );
 }
---
+++ src/components/QuestinonCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function QuestinonCard() {
+  return (
+    <div>QuestinonCard</div>
+  )
+}
+
